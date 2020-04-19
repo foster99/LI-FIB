a864b9056b4cb7e1f52932e4597956935be4c40a
foster99 <edgarperezblanco@gmail.com>
2020-04-19 17:49:28 +0200

    listo para programar

--- 04-Practica4/Gangsters/gangsters.pl
+++ 04-Practica4/Gangsters/gangsters.pl
@@ -1,3 +1,6 @@
+:-dynamic(varNumber / 3).
+symbolicOutput(0).  % set to 1 to see symbolic output only; 0 otherwise.
+
 %% The mafia has a lot gangsters for doing different tasks.
 %% These tasks are planned every 3 days (72h), according to a forecast
 %% of the tasks to be done every hour.
@@ -27,7 +30,6 @@
 %% gangster g12: kkkkkkkkkkkkkkkkkk-kkkkkkkkkkkkkkkkkk-kkkkkkkkkkkkkkkkkk-kkkkkkkkkkkkkkk
 
 
-
 %%%%%%%%%%%%%%%%%%%%% INPUT:
 
 % example: 4 gangsters are needed for killing on hour 1, one gangster on hour 2, two gangsters on hour 3, etc.
@@ -55,6 +57,33 @@ blocked(G,H):-   notAvailable(G,L), member(H,L).
 available(G,H):- hour(H), gangster(G), \+blocked(G,H).
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1.- Declare SAT variables to be used
+
+satVariable( does(G,T,H) )  :- gangster(G), task(T), hour(H).   % Gangster G does task T at hour H.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2. This predicate writeClauses(MaxCost) generates the clauses that guarantee that
+% a solution with cost at most MaxCost is found
+
+writeClauses(infinite):- !, writeClauses(72),!.
+writeClauses(K):-
+    
+
+    true,!.
+writeClauses(_):- told, nl, write('writeClauses failed!'), nl,nl, halt.
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 3. This predicate displays a given solution M:
+
 % We use (at least) the following types of symbolic propositional variables:
 %   1. does(G,T,H) means:  "gangster G does task T at hour H"     (MANDATORY)
 %   2. ...
@@ -69,4 +98,149 @@ writeIfBusy(G,H,M):- member( does(G, killing,       H), M),  write('k'),!.
 writeIfBusy(G,H,M):- member( does(G, countingMoney, H), M),  write('c'),!.
 writeIfBusy(G,H,M):- member( does(G, politics,      H), M),  write('p'),!.
 writeIfBusy(_,_,_):- write('-'),!.
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 4. This predicate computes the cost of a given solution M:
+
+costOfThisSolution(M,Cost):- findall(C,member(x(_,C),M),L), sort(L,L1), length(L1,Cost), !.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% No need to modify anything beow this line:
+
+main:-  symbolicOutput(1), !, writeClauses(infinite), halt.   % print the clauses in symbolic form and halt
+main:-
+    told, write('Looking for initial solution with arbitrary cost...'), nl,
+    initClauseGeneration,
+    tell(clauses), writeClauses(infinite), told,
+    tell(header),  writeHeader,  told,
+    numVars(N), numClauses(C), 
+    write('Generated '), write(C), write(' clauses over '), write(N), write(' variables. '),nl,
+    shell('cat header clauses > infile.cnf',_),
+    write('Launching picosat...'), nl,
+    shell('picosat -v -o model infile.cnf', Result),  % if sat: Result=10; if unsat: Result=20.
+    treatResult(Result,[]),!.
+
+treatResult(20,[]       ):- write('No solution exists.'), nl, halt.
+treatResult(20,BestModel):-
+    nl,nl, costOfThisSolution(BestModel,Cost), write('Unsatisfiable. So the optimal solution was this one with cost '),
+    write(Cost), write(':'), nl, displaySol(BestModel), halt.
+treatResult(10,_):- %   shell('cat model',_),
+    write('Solution found '), flush_output,
+    see(model), symbolicModel(M), seen,
+    costOfThisSolution(M,Cost),
+    write('with cost '), write(Cost), nl,nl,
+    displaySol(M), 
+    Cost1 is Cost-1,   nl,nl,nl,nl,nl,  write('Now looking for solution with cost '), write(Cost1), write('...'), nl,
+    initClauseGeneration, tell(clauses), writeClauses(Cost1), told,
+    tell(header),  writeHeader,  told,
+    numVars(N),numClauses(C),
+    write('Generated '), write(C), write(' clauses over '), write(N), write(' variables. '),nl,
+    shell('cat header clauses > infile.cnf',_),
+    write('Launching picosat...'), nl,
+    shell('picosat -v -o model infile.cnf', Result),  % if sat: Result=10; if unsat: Result=20.
+    treatResult(Result,M),!.
+treatResult(_,_):- write('cnf input error. Wrote something strange in your cnf?'), nl,nl, halt.
+    
+
+initClauseGeneration:-  %initialize all info about variables and clauses:
+	retractall(numClauses(   _)),
+	retractall(numVars(      _)),
+	retractall(varNumber(_,_,_)),
+	assert(numClauses( 0 )),
+	assert(numVars(    0 )),     !.
+
+writeClause([]):- symbolicOutput(1),!, nl.
+writeClause([]):- countClause, write(0), nl.
+writeClause([Lit|C]):- w(Lit), writeClause(C),!.
+w(-Var):- symbolicOutput(1), satVariable(Var), write(-Var), write(' '),!. 
+w( Var):- symbolicOutput(1), satVariable(Var), write( Var), write(' '),!. 
+w(-Var):- satVariable(Var),  var2num(Var,N),   write(-), write(N), write(' '),!.
+w( Var):- satVariable(Var),  var2num(Var,N),             write(N), write(' '),!.
+w( Lit):- told, write('ERROR: generating clause with undeclared variable in literal '), write(Lit), nl,nl, halt.
+
+
+% given the symbolic variable V, find its variable number N in the SAT solver:
+var2num(V,N):- hash_term(V,Key), existsOrCreate(V,Key,N),!.
+existsOrCreate(V,Key,N):- varNumber(Key,V,N),!.                            % V already existed with num N
+existsOrCreate(V,Key,N):- newVarNumber(N), assert(varNumber(Key,V,N)), !.  % otherwise, introduce new N for V
+
+writeHeader:- numVars(N),numClauses(C), write('p cnf '),write(N), write(' '),write(C),nl.
+
+countClause:-     retract( numClauses(N0) ), N is N0+1, assert( numClauses(N) ),!.
+newVarNumber(N):- retract( numVars(   N0) ), N is N0+1, assert(    numVars(N) ),!.
+
+% Getting the symbolic model M from the output file:
+symbolicModel(M):- get_code(Char), readWord(Char,W), symbolicModel(M1), addIfPositiveInt(W,M1,M),!.
+symbolicModel([]).
+addIfPositiveInt(W,L,[Var|L]):- W = [C|_], between(48,57,C), number_codes(N,W), N>0, varNumber(_,Var,N),!.
+addIfPositiveInt(_,L,L).
+readWord( 99,W):- repeat, get_code(Ch), member(Ch,[-1,10]), !, get_code(Ch1), readWord(Ch1,W),!. % skip line starting w/ c
+readWord(115,W):- repeat, get_code(Ch), member(Ch,[-1,10]), !, get_code(Ch1), readWord(Ch1,W),!. % skip line starting w/ s
+readWord(-1,_):-!, fail. %end of file
+readWord(C,[]):- member(C,[10,32]), !. % newline or white space marks end of word
+readWord(Char,[Char|W]):- get_code(Char1), readWord(Char1,W), !.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Everything below is given as a standard library, reusable for solving 
+%    with SAT many different problems.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Express that Var is equivalent to the disjunction of Lits:
+expressOr( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> or('), write(Lits), write(')'), nl, !. 
+expressOr( Var, Lits ):- member(Lit,Lits), negate(Lit,NLit), writeClause([ NLit, Var ]), fail.
+expressOr( Var, Lits ):- negate(Var,NVar), writeClause([ NVar | Lits ]),!.
+
+% Express that Var is equivalent to the conjunction of Lits:
+expressAnd( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> and('), write(Lits), write(')'), nl, !. 
+expressAnd( Var, Lits):- member(Lit,Lits), negate(Var,NVar), writeClause([ NVar, Lit ]), fail.
+expressAnd( Var, Lits):- findall(NLit, (member(Lit,Lits), negate(Lit,NLit)), NLits), writeClause([ Var | NLits]), !.
+
+
+%%%%%% Cardinality constraints on arbitrary sets of literals Lits:
+
+exactly(K,Lits):- symbolicOutput(1), write( exactly(K,Lits) ), nl, !.
+exactly(K,Lits):- atLeast(K,Lits), atMost(K,Lits),!.
+
+atMost(K,Lits):- symbolicOutput(1), write( atMost(K,Lits) ), nl, !.
+atMost(K,Lits):-   % l1+...+ln <= k:  in all subsets of size k+1, at least one is false:
+	negateAll(Lits,NLits),
+	K1 is K+1,    subsetOfSize(K1,NLits,Clause), writeClause(Clause),fail.
+atMost(_,_).
+
+atLeast(K,Lits):- symbolicOutput(1), write( atLeast(K,Lits) ), nl, !.
+atLeast(K,Lits):-  % l1+...+ln >= k: in all subsets of size n-k+1, at least one is true:
+	length(Lits,N),
+	K1 is N-K+1,  subsetOfSize(K1, Lits,Clause), writeClause(Clause),fail.
+atLeast(_,_).
+
+negateAll( [], [] ).
+negateAll( [Lit|Lits], [NLit|NLits] ):- negate(Lit,NLit), negateAll( Lits, NLits ),!.
+
+negate( -Var,  Var):-!.
+negate(  Var, -Var):-!.
+
+subsetOfSize(0,_,[]):-!.
+subsetOfSize(N,[X|L],[X|S]):- N1 is N-1, length(L,Leng), Leng>=N1, subsetOfSize(N1,L,S).
+subsetOfSize(N,[_|L],   S ):-            length(L,Leng), Leng>=N,  subsetOfSize( N,L,S).
 
